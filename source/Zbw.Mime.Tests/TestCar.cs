225541f8c03540b915b0a74796ff6d0ed5d25073
memi13 <michael.megroz@gmail.com>
2019-09-08 14:31:17 +0200

    first Testes

---
+++ source/Zbw.Mime.Tests/TestCar.cs
@@ -0,0 +1,15 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+
+namespace Zbw.Mime.Tests
+{
+    [TestFixture]
+    public class TestCar
+    {
+        [Test]
+        public void Test1()
+        {
+            var foo = A.Fake<ICarService>(x => x.Strict());
+        }
+    }
+}
---
+++ source/Zbw.Mime.TestsCarRent/TestCar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using ZbW.CarRentify.CarManagement.Api;
+using ZbW.CarRentify.CarManagement.Domain;
+using ZbW.CarRentify.CarManagement.Services;
+
+namespace Zbw.Mime.TestsCarRent
+{
+    [TestFixture]
+    public class TestCar
+    {
+        [Test]
+        public void Test1()
+        {
+            var newList=new List<Car>();
+            newList.Add(new Car());
+            
+             var foo = A.Fake<ICarService>(x => x.Strict());
+             A.CallTo(() => foo.Get()).Returns(newList);
+             var t = new CarController(foo);
+             var dd= t.Get().First();
+
+            Assert.That(dd, Is.Empty);
+        }
+
+    }
+}
